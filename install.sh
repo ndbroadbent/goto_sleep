d9f49d25dd4d1d8bd2ccec0820b5f67f048fc977
Nathan Broadbent <ndbroadbent@users.noreply.github.com>
2016-05-16 01:25:41 +0700

    Better logging in install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 # Install bundler
-type bundle > /dev/null 2>&1 || gem install bundler
+type bundle > /dev/null 2>&1 || {
+  echo "Installing bundler..."
+  gem install bundler
+}
 
 # Install gem dependencies
+echo "Checking gem dependencies..."
 bundle install --quiet
 
 # Show and write crontab
+echo "=== Installing crontab for user '$USER':"
 whenever -f notification_schedule.rb
-whenever -f shutdown_schedule.rb
-
-echo -e "\nInstalling notification tasks for current user..."
 whenever -f notification_schedule.rb -w
-echo "Installing shutdown task for root user..."
+
+echo -e "\n=== Installing crontab for root user:"
+whenever -f shutdown_schedule.rb
 sudo whenever -f shutdown_schedule.rb -w
 
 echo -e "\nYou're all set! Edit config/schedule.rb if you want to change the SHUTDOWN_TIME."
