ee22b8c72d9f4c60325a764fdc37d6d90280e38d
Nathan Broadbent <ndbroadbent@users.noreply.github.com>
2016-05-20 01:28:57 +0700

    I forgot to do this, may as well add it

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ type gem > /dev/null 2>&1 || {
   echo
   echo "  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
   echo "  \curl -sSL https://get.rvm.io | bash -s stable --ruby"
+  echo "  source $HOME/.rvm/scripts/rvm"
   echo
   echo "Read more about RVM at: https://rvm.io/rvm/install"
   echo
