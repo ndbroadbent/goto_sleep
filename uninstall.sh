40635a32545ebb55e8a998b0f93d1e57efe55520
Nathan Broadbent <ndbroadbent@users.noreply.github.com>
2016-05-20 02:32:12 +0700

    Damn, linux makes sudo pretty hard

--- uninstall.sh
+++ uninstall.sh
@@ -2,4 +2,4 @@
 echo "Removing notification tasks for current user..."
 whenever -f notification_schedule.rb -c
 echo "Removing shutdown task for root user..."
-sudo -E whenever -f shutdown_schedule.rb -c
+sudo -E env "PATH=$PATH" whenever -f shutdown_schedule.rb -c
