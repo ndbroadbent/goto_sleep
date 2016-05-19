75d31de3a0732b11223c8ff690cafe9d1d4d08b7
Nathan Broadbent <ndbroadbent@users.noreply.github.com>
2016-05-19 21:14:26 +0700

    Better logging in install script

--- install.sh
+++ install.sh
@@ -11,12 +11,16 @@ echo "Checking gem dependencies..."
 bundle install --quiet
 
 # Show and write crontab
-echo "=== Installing crontab for user '$USER':"
+echo "Showing crontab for user '$USER':"
+echo "---------------------------------------------"
 whenever -f notification_schedule.rb
+echo -e "\n=== Installing crontab for user '$USER':"
 whenever -f notification_schedule.rb -w
 
-echo -e "\n=== Installing crontab for root user:"
+echo -e "\nShowing crontab for root user:"
+echo      "---------------------------------"
 whenever -f shutdown_schedule.rb
+echo -e "\n=== Installing crontab for root user:"
 sudo whenever -f shutdown_schedule.rb -w
 
 echo -e "\nYou're all set! Edit config/schedule.rb if you want to change the SHUTDOWN_TIME."
