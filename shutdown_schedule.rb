bafb2c3e6381b7d6708830e7ffbb7220130b6da7
Nathan Broadbent <ndbroadbent@users.noreply.github.com>
2016-11-25 15:57:26 +0700

    Just sleep, shutdown is too annoying. have to start everything again, and some programs are too stupid to wait for a wifi connection, so they just sign out.

--- shutdown_schedule.rb
+++ shutdown_schedule.rb
@@ -3,5 +3,5 @@ require 'yaml'
 config = YAML.load_file('config.yml')
 
 every :day, :at => config['shutdown_time'] do
-  command 'sleep 5; /sbin/shutdown -h now'
+  command 'sleep 5; /sbin/shutdown -s now'
 end
